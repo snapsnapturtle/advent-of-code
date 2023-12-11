cd624aab99249b646c8aeefdf64ca148154260d8
Jonah Möller <jonah@closelink.net>
2023-12-11 16:14:46 +0100

    add day 11

---
+++ 2023/day-11/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"math"
+	"regexp"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running Part:", part)
+
+	if part == 1 {
+		ans := partOne(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := partTwo(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func partOne(input string) int {
+	universe := strings.Split(input, "\n")
+
+	return calculateShortestDistances(universe, 2)
+}
+
+func partTwo(input string) int {
+	universe := strings.Split(input, "\n")
+
+	return calculateShortestDistances(universe, 1_000_000)
+}
+
+func calculateShortestDistances(universe []string, emptySpaceValue int) int {
+	galaxies := findGalaxies(universe)
+	rowIds, colIds := findExpandingSpaces(universe)
+
+	totalDistance := 0
+	pairs := 0
+
+	for index, galaxy := range galaxies {
+		for i := index + 1; i < len(galaxies); i++ {
+			pairs++
+			distantGalaxy := galaxies[i]
+
+			distanceX := math.Abs(float64(galaxy[0] - distantGalaxy[0]))
+			distanceY := math.Abs(float64(galaxy[1] - distantGalaxy[1]))
+
+			// check if connection crosses through rowIds
+			for _, rowId := range rowIds {
+				if (rowId > galaxy[0] && rowId < distantGalaxy[0]) || (rowId < galaxy[0] && rowId > distantGalaxy[0]) {
+					totalDistance += emptySpaceValue - 1
+				}
+			}
+
+			// check if connection crosses through colIds
+			for _, colId := range colIds {
+				if (colId > galaxy[1] && colId < distantGalaxy[1]) || (colId < galaxy[1] && colId > distantGalaxy[1]) {
+					totalDistance += emptySpaceValue - 1
+				}
+			}
+
+			totalDistance += int(distanceX + distanceY)
+		}
+	}
+
+	return totalDistance
+}
+
+func findGalaxies(universe []string) [][2]int {
+	var galaxies [][2]int
+
+	for rowIndex, row := range universe {
+		for colIndex, col := range row {
+			if col == '#' {
+				galaxies = append(galaxies, [2]int{rowIndex, colIndex})
+			}
+		}
+	}
+
+	return galaxies
+}
+
+func findExpandingSpaces(universe []string) (rowIndices []int, colIndices []int) {
+	var rowIds []int
+	var colIds []int
+
+	galaxyRunes := make(map[int]bool)
+
+	for rowIndex, row := range universe {
+		galaxyRuneRegex := regexp.MustCompile(`(#)`)
+		galaxyRuneMatches := galaxyRuneRegex.FindAllStringIndex(row, -1)
+
+		if len(galaxyRuneMatches) > 0 {
+			for _, match := range galaxyRuneMatches {
+				galaxyRuneIndex := match[0]
+				galaxyRunes[galaxyRuneIndex] = true
+			}
+		} else {
+			rowIds = append(rowIds, rowIndex)
+		}
+	}
+
+	for colIndex := range universe[0] {
+		if _, ok := galaxyRunes[colIndex]; !ok {
+			colIds = append(colIds, colIndex)
+		}
+	}
+
+	return rowIds, colIds
+}
