fd221e80e5f3dd0394a93c1d6287f113f4bebbdb
Jonah Möller <jonah@closelink.net>
2024-01-18 15:45:54 +0100

    add tests for day 1

--- 2022/day-01/day_01.go
+++ 2022/day-01/day_01.go
@@ -1,42 +1,14 @@
-package main
+package day_01
 
 import (
 	_ "embed"
-	"flag"
-	"fmt"
+	"snapsnapturtle/advent-of-code/util"
 	"sort"
 	"strconv"
-	"strings"
 )
 
-//go:embed input.txt
-var input string
-
-func init() {
-	// do this in init (not main) so test file has same input
-	input = strings.TrimRight(input, "\n")
-	if len(input) == 0 {
-		panic("empty input.txt file")
-	}
-}
-
-func main() {
-	var part int
-	flag.IntVar(&part, "part", 1, "part 1 or 2")
-	flag.Parse()
-	fmt.Println("Running Part: ", part)
-
-	if part != 1 {
-		ans := partOne(input)
-		fmt.Println("Output: ", ans)
-	} else {
-		ans := partTwo(input)
-		fmt.Println("Output:", ans)
-	}
-}
-
-func partOne(input string) int {
-	lines := strings.Split(input, "\n")
+func PartOne(input string) int {
+	lines := util.ParseLinesFromInput(input)
 
 	maxCalories := 0
 	currentElfCalories := 0
@@ -59,8 +31,8 @@ func partOne(input string) int {
 	return maxCalories
 }
 
-func partTwo(input string) int {
-	lines := strings.Split(input, "\n")
+func PartTwo(input string) int {
+	lines := util.ParseLinesFromInput(input)
 
 	var calorieCounts []int
 	currentElfCalories := 0
