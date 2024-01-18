4de77cba0b0dd8968d63c4d3fada0eb91ec231a3
Jonah Möller <jonah@closelink.net>
2024-01-18 15:55:14 +0100

    add tests for day 4

--- 2022/day-04/day_04.go
+++ 2022/day-04/day_04.go
@@ -1,42 +1,14 @@
-package main
+package day_04
 
 import (
 	_ "embed"
-	"flag"
-	"fmt"
 	"regexp"
+	"snapsnapturtle/advent-of-code/util"
 	"strconv"
-	"strings"
 )
 
-//go:embed input.txt
-var input string
-
-func init() {
-	// do this in init (not main) so test file has same input
-	input = strings.TrimRight(input, "\n")
-	if len(input) == 0 {
-		panic("empty input.txt file")
-	}
-}
-
-func main() {
-	var part int
-	flag.IntVar(&part, "part", 1, "part 1 or 2")
-	flag.Parse()
-	fmt.Println("Running Part: ", part)
-
-	if part == 1 {
-		ans := partOne(input)
-		fmt.Println("Output: ", ans)
-	} else {
-		ans := partTwo(input)
-		fmt.Println("Output:", ans)
-	}
-}
-
-func partOne(input string) int {
-	lines := strings.Split(input, "\n")
+func PartOne(input string) int {
+	lines := util.ParseLinesFromInput(input)
 
 	var overlappingRanges int
 
@@ -57,8 +29,8 @@ func partOne(input string) int {
 	return overlappingRanges
 }
 
-func partTwo(input string) int {
-	lines := strings.Split(input, "\n")
+func PartTwo(input string) int {
+	lines := util.ParseLinesFromInput(input)
 
 	var overlappingRanges int
 
---
+++ 2022/day-04/day_04_test.go
@@ -0,0 +1,54 @@
+package day_04
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed input.txt
+var actualInput string
+
+//go:embed example.txt
+var exampleInput string
+
+func TestPartOne(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{exampleInput}, 2},
+		{"actual", args{actualInput}, 556},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PartOne(tt.args.input); got != tt.want {
+				t.Errorf("PartOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{exampleInput}, 4},
+		{"actual", args{actualInput}, 876},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PartTwo(tt.args.input); got != tt.want {
+				t.Errorf("PartTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
