36c244665274328f576af02c5bdc2498a5ca2041
Jonah Möller <jonah.moeller@googlemail.com>
2024-12-08 11:51:35 +0100

    add day 2 - part 1

---
+++ 2024/day-02/day_02.go
@@ -0,0 +1,63 @@
+package day_01
+
+import (
+	"snapsnapturtle/advent-of-code/util"
+	"strconv"
+	"strings"
+)
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func PartOne(input string) int {
+	reports := util.ParseLinesFromInput(input)
+	safeReports := 0
+
+	for _, report := range reports {
+		levels := strings.Fields(report)
+		isValidReport := true
+
+		previousLevelValue, _ := strconv.Atoi(levels[0])
+		previousLevelDifference := 0
+
+		for i, level := range levels {
+			if i == 0 {
+				continue
+			}
+
+			levelValue, _ := strconv.Atoi(level)
+
+			absoluteDifference := Abs(levelValue - previousLevelValue)
+
+			if absoluteDifference < 1 || absoluteDifference > 3 {
+				isValidReport = false
+				break
+			}
+
+			difference := levelValue - previousLevelValue
+
+			if (previousLevelDifference > 0 && difference < 0) || previousLevelDifference < 0 && difference > 0 {
+				isValidReport = false
+				break
+			}
+
+			previousLevelValue = levelValue
+			previousLevelDifference = difference
+		}
+
+		if isValidReport {
+			safeReports++
+		}
+	}
+
+	return safeReports
+}
+
+func PartTwo(input string) int {
+	// todo: implement
+	return 0
+}
